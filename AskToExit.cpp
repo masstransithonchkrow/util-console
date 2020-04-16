b714332c008711c202a3b02ab371e01b647450ba
Eric Shields <10548400+masstransithonchkrow@users.noreply.github.com>
2020-04-16 08:37:35 -0400

    better input method
    this way, an #include<string> header is not needed. It seems that the input from AskToExit is retained in the repeated prompt. Using the precedent cin.ignore included in this code BEFORE the repeated prompt helps.

--- AskToExit.cpp
+++ AskToExit.cpp
@@ -1,13 +1,16 @@
+//You should add this before the prompt you wish to repeat.
+  cin.ignore(1,'\n'); //before the prompt seems to work best. I like what I've done.
+
 //ask to exit function
 //asks user if they wish to leave the program.
 //should be preceded by a function prototype.
 int AskToExit()
 {
-	string input;
+	char input;
     cout << "Do you want to run the program again? Type 'y' if you do, or any (alphanumeric) key to leave. ";
         cin >> input;
-        if (input == "y")
+        if (input == 'y')
             program();
-        else if (input != "y")
+        else if (input != 'y')
             return 0;
 }
